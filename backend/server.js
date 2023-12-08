74b16fdabba42cf6d53c69c65c545f442d9f2c49
Nitesh-NEU <more.ni@northeastern.edu>
2023-12-07 20:07:30 -0500

    fix-1

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,18 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes  from './routes/uploadRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
 
-// Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded( {extended: true} ));
-
-//Cookie parser middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
